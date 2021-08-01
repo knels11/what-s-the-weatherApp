e469d68748d85ad0277a45d1d860b2cede1e1f9f
kiehp <kierra.nelson@aol.com>
2021-08-01 13:12:51 -0400

    successful api req + bootstrap

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const url= 'https://www.weatherapi.com/my/91b29ef9b94649bc8fe162716210108'
+const apiKey= '91b29ef9b94649bc8fe162716210108'
+
+var getWeather = function(json) {
+    document.getElementById('todays-weather').innerHTML = 
+    json.stringify(json, undefined, 2);
+};
+
+fetch(url)
+.then (function(response) {
+    return response.json();
+})
+.then (function(weather) {
+getWeather(weather);
+})
+.catch (function(error) {
+    console.log(error);
+});
+
 function getLocation() {
     if (navigator.geolocation) {
     //pass in show position() function to get current position
@@ -6,31 +25,4 @@ function getLocation() {
     else {
         alert("geolocation is not supported by this browser.");
     }
-}
-
-var makeHTML = function() {
-    document.getElementById('todaysWeather').innerHTML=
-    JSON.stringify(json, undefined, 2);
 };
-//fetch 
-fetch ("https://api.weatherbit.io/v2.0/current")
-.then(function(response) {
-    return response.json();
-})
-.then(function(localWeather) {
-    makeHTML(localWeather);
-})
-//validate request
-.catch(function(error) {
-    console.log(error);
-});
-
-//after getCurrentPosition(), this will run
-function showPosition(position) {
-    //get coordinates from the given object
-    var lat = position.coords.latitude;
-    var lon = postion.coords.longitude;
-    console.log("your coordinates are latitude:" + lat + "longitude" + lon);
-    //call the next function, passing in the coords
-    showPosition();
-}
