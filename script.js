7a6d9331516b10d579d247f36365f822fd810788
kiehp <kierra.nelson@aol.com>
2021-08-08 13:27:30 -0400

    GET fetch requested

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 const url= 'https://www.weatherapi.com/my/91b29ef9b94649bc8fe162716210108'
 const apiKey= '91b29ef9b94649bc8fe162716210108'
 
-var getWeather = function(json) {
-    document.getElementById('todays-weather').innerHTML = 
+var makeHTML = function(json) {
+    document.getElementById('json').innerHTML = 
     json.stringify(json, undefined, 2);
 };
 
-fetch(url)
+fetch("https://www.weatherapi.com/my/91b29ef9b94649bc8fe162716210108")
 .then (function(response) {
     return response.json();
 })
 .then (function(weather) {
 getWeather(weather);
-})
-.catch (function(error) {
-    console.log(error);
 });
-
-function getLocation() {
-    if (navigator.geolocation) {
-    //pass in show position() function to get current position
-    navigator.geolocation.getCurrentPosition(showPosition);
-    }
-    else {
-        alert("geolocation is not supported by this browser.");
-    }
-};
