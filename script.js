179037c9a06b3f56265d838eecd601a0cce60b24
kiehp <kierra.nelson@aol.com>
2021-08-08 13:43:01 -0400

    api openweather map

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const url= 'https://www.weatherapi.com/my/91b29ef9b94649bc8fe162716210108'
-const apiKey= '91b29ef9b94649bc8fe162716210108'
+//const url= 'https://www.weatherapi.com/my/91b29ef9b94649bc8fe162716210108'
+//const apiKey= '91b29ef9b94649bc8fe162716210108'
 
 var makeHTML = function(json) {
     document.getElementById('json').innerHTML = 
     json.stringify(json, undefined, 2);
 };
 
-fetch("https://www.weatherapi.com/my/91b29ef9b94649bc8fe162716210108")
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
 .then (function(response) {
     return response.json();
 })
-.then (function(weather) {
-getWeather(weather);
-});
+.then (function(response) {
+return(response);
+}); 
+//if req isnt working validate code
